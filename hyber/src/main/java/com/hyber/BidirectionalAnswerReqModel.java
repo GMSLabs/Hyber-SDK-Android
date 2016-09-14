102508fe3430d4650e86cadb38c746f26d3169c7
Andrew Kochura <alexeevi40327@gmail.com>
2016-09-14 13:13:43 +0300

    Remove bidir answer sent time

--- hyber/src/main/java/com/hyber/BidirectionalAnswerReqModel.java
+++ hyber/src/main/java/com/hyber/BidirectionalAnswerReqModel.java
@@ -12,17 +12,13 @@ public class BidirectionalAnswerReqModel {
     @SerializedName("text")
     private String mAnswerText;
 
-    @SerializedName("time")
-    private Long mSentAt;
-
     public BidirectionalAnswerReqModel() {
 
     }
 
-    public BidirectionalAnswerReqModel(@NonNull String messageId, @NonNull String answerText, @NonNull Long sentAt) {
+    public BidirectionalAnswerReqModel(@NonNull String messageId, @NonNull String answerText) {
         this.mMessageId = messageId;
         this.mAnswerText = answerText;
-        this.mSentAt = sentAt;
     }
 
 }
