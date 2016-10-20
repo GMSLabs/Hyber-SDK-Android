2330776897c02ebf539d0bf4f2e27632d50d5f6f
andrewkochura <alexeevi40327@gmail.com>
2016-10-20 11:05:45 +0300

    Change version to release 2.0.0

--- hyber/src/main/java/com/hyber/HyberSdkVersion.java
+++ hyber/src/main/java/com/hyber/HyberSdkVersion.java
@@ -1,7 +1,7 @@
 package com.hyber;
 
 final class HyberSdkVersion {
-    public static final String BUILD = "2.0.0-SNAPSHOT";
+    public static final String BUILD = "2.0.0";
 
     private HyberSdkVersion() {
 
