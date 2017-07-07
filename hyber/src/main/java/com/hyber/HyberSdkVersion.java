ee0f68ffc20cbf3b3caa9b1c3cf46b4f6518c5b9
Andrew Kochura <alexeevi40327@gmail.com>
2017-07-07 21:31:16 +0300

    - Android API level update from 25 to 26; - Android build gradle plugin update from 2.3.2 to 2.3.3; - Android support library update from 25.3.1 to 25.4.0; - Butterknife update from 8.4.0 to 8.7.0;

--- hyber/src/main/java/com/hyber/HyberSdkVersion.java
+++ hyber/src/main/java/com/hyber/HyberSdkVersion.java
@@ -1,7 +1,7 @@
 package com.hyber;
 
 final class HyberSdkVersion {
-    public static final String BUILD = "2.2.2";
+    public static final String BUILD = "2.2.3";
 
     private HyberSdkVersion() {
 
